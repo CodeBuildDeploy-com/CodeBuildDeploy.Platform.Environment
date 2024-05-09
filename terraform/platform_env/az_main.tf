e06f9949f3d81ed1d3f664ed87cf1773db8ae513
Mark Pollard <mark.pollard@bjss.co.uk>
2024-05-09 10:36:33 +0100

    CBD-28: Add AKS Cluster to platform envs

--- terraform/platform_env/az_main.tf
+++ terraform/platform_env/az_main.tf
@@ -1,13 +1,13 @@
-resource "azurerm_resource_group" "cbd_env_rg" {
+resource "azurerm_resource_group" "cbd_plat_rg" {
   name     = "cbd-${var.platform_env}-rg"
   location = var.default_location
   tags     = local.tags
 }
 
-resource "azurerm_log_analytics_workspace" "cbd_env_law" {
+resource "azurerm_log_analytics_workspace" "cbd_plat_law" {
   name                = "cbd-${var.platform_env}-law"
-  resource_group_name = azurerm_resource_group.cbd_env_rg.name
-  location            = azurerm_resource_group.cbd_env_rg.location
+  resource_group_name = azurerm_resource_group.cbd_plat_rg.name
+  location            = azurerm_resource_group.cbd_plat_rg.location
   sku                 = "PerGB2018"
   retention_in_days   = 90
   tags                = local.tags
